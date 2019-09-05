86ea2a85e180821b3d6ce351e0bcd756fb81a5fd
Ilham Nasution <ilhaamrahman@gmail.com>
2019-09-05 17:28:27 +0700

    Bottom Navbar Added

--- App.js
+++ App.js
@@ -1,114 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React from 'react';
+import {View, Text, Image} from 'react-native';
 
 const App = () => {
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
+    <View style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <Text>Bagian Content</Text>
+      </View>
+      <View
+        style={{
+          height: 54,
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Image
+            style={{width: 26, height: 26}}
+            source={require('./image/home.png')}
+          />
+          <Text
+            style={{
+              fontSize: 10,
+              color: '#545454',
+              marginTop: 4,
+              color: '#43AB4A',
+            }}>
+            Home
+          </Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Image
+            style={{width: 26, height: 26}}
+            source={require('./image/order.png')}
+          />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+            Orders
+          </Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Image
+            style={{width: 26, height: 26}}
+            source={require('./image/help.png')}
+          />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+            Help
+          </Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Image
+            style={{width: 26, height: 26}}
+            source={require('./image/inbox.png')}
+          />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+            Inbox
+          </Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Image
+            style={{width: 26, height: 26}}
+            source={require('./image/account.png')}
+          />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+            Account
+          </Text>
+        </View>
+      </View>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
